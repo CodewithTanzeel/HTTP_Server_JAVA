b1f15576169498f94ff33f45989bda4908b6bbb5
CodewithTanzeel <Tanzeelofficial@outlook.com>
2025-09-13 01:06:24 +0500

    ,

---
+++ HTTPSERVER/src/main/java/com/codefromscratch/httpserver/HttpServer.java
@@ -0,0 +1,10 @@
+package com.codefromscratch.httpserver;
+/*
+        Driver class for the HTTPServer
+ */
+
+public class HttpServer {
+      public static void main(String[] args){
+          System.out.println("HTTP Server Started");
+      }
+}
